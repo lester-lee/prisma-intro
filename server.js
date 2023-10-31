5ed014d267e935a5df18bb8c1d873d60e1bcadeb
Lester Lee <contact@lester-lee.com>
2023-10-31 16:16:35 -0400

    Add scripts to package.json

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use('/api', require('./api'));
 
 // Simple error handling middleware
 app.use((err, req, res, next) => {
+  console.error(err);
   const status = err.status ?? 500;
   const message = err.message ?? 'Internal server error.';
   res.status(status).json({ message });
